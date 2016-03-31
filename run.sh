590258cf77ab64ebe74d0cd338d8d26ee9f99d79
Kenny Bastani <kb@socialmoon.com>
2016-03-31 00:24:42 -0700

    Integrated config service

--- run.sh
+++ run.sh
@@ -2,7 +2,36 @@
 
 set -e
 
+# Build the project and docker images
 mvn clean install
+
+# Export the active docker machine IP
 export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+
+# Start the config service first and wait for it to become available
+docker-compose up -d config-service
+
+while [ -z ${CONFIG_SERVICE_READY} ]; do
+  echo "Waiting for config service..."
+  if [ "$(nc $DOCKER_IP -z -w 4 8888; echo $?)" = 0 ]; then
+      CONFIG_SERVICE_READY=true;
+  fi
+  sleep 2
+done
+
+# Start the discovery service next and wait
+docker-compose up -d discovery-service
+
+while [ -z ${DISCOVERY_SERVICE_READY} ]; do
+  echo "Waiting for discovery service..."
+  if [ "$(nc $DOCKER_IP -z -w 4 8761; echo $?)" = 0 ]; then
+      DISCOVERY_SERVICE_READY=true;
+  fi
+  sleep 2
+done
+
+# Start the other containers
 docker-compose up -d
+
+# Attach to the log output of the cluster
 docker-compose logs
