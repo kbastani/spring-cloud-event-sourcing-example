259c2989323524188fe7cb80cd70fcfce36881d9
Barry Commins <barry.commins@gmail.com>
2016-05-04 00:09:39 +0100

    Updated run.sh to support Windows/Cygwin
    netcat does not exist in the default Cygwin install, but curl does.
    Curling actuator endpoint to check service status

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ docker-compose up -d config-service
 
 while [ -z ${CONFIG_SERVICE_READY} ]; do
   echo "Waiting for config service..."
-  if [ "$(nc $DOCKER_IP -z -w 4 8888; echo $?)" = 0 ]; then
+  if [ "$(curl --silent $DOCKER_IP:8888/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       CONFIG_SERVICE_READY=true;
   fi
   sleep 2
@@ -31,7 +31,7 @@ docker-compose up -d discovery-service
 
 while [ -z ${DISCOVERY_SERVICE_READY} ]; do
   echo "Waiting for discovery service..."
-  if [ "$(nc $DOCKER_IP -z -w 4 8761; echo $?)" = 0 ]; then
+  if [ "$(curl --silent $DOCKER_IP:8761/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       DISCOVERY_SERVICE_READY=true;
   fi
   sleep 2
